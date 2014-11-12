95b06349de8813ad40d40f79c364be4fe7abde29
SanCoder-Q <sancoder.q@gmail.com>
2014-11-12 13:05:43 +0800

    Get an correct empty inventory

---
+++ pos_v1/spec/main-spec.js
@@ -0,0 +1,64 @@
+describe('pos', function () {
+  var allItems;
+  var inputs;
+
+  it('should print an empty inventory when get a empty array input', function(){
+
+    allItems = loadAllItems();
+    inputs = [];
+
+    spyOn(console, 'log');
+
+    printInventory(inputs);
+
+    var expectText =
+      '***<没钱赚商店>购物清单***\n' +
+      '----------------------\n' +
+      '挥泪赠送商品：\n' +
+      '----------------------\n' +
+      '总计：0.00(元)\n' +
+      '节省：0.00(元)\n' +
+      '**********************';
+
+    expect(console.log).toHaveBeenCalledWith(expectText);
+
+  });
+
+
+/*
+    it('should print correct text', function () {
+        allItems = loadAllItems();
+        inputs = [
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000003-2',
+            'ITEM000005',
+            'ITEM000005',
+            'ITEM000005'
+        ];
+
+        spyOn(console, 'log');
+
+        printInventory(inputs);
+
+        var expectText =
+            '***<没钱赚商店>购物清单***\n' +
+            '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
+            '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
+            '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
+            '----------------------\n' +
+            '挥泪赠送商品：\n' +
+            '名称：雪碧，数量：1瓶\n' +
+            '名称：方便面，数量：1袋\n' +
+            '----------------------\n' +
+            '总计：51.00(元)\n' +
+            '节省：7.50(元)\n' +
+            '**********************';
+
+          expect(console.log).toHaveBeenCalledWith(expectText);
+    });*/
+
+});
