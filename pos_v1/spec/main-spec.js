c2622635dec0e534d05890ff8ed9def791f06142
SanCoder-Q <sancoder.q@gmail.com>
2014-11-12 13:36:25 +0800

    Get an correct inventory when input [ITEM000002]

--- pos_v1/spec/main-spec.js
+++ pos_v1/spec/main-spec.js
@@ -1,11 +1,8 @@
 describe('pos', function () {
-  var allItems;
-  var inputs;
 
   it('should print an empty inventory when get a empty array input', function(){
 
-    allItems = loadAllItems();
-    inputs = [];
+    var inputs = [];
 
     spyOn(console, 'log');
 
@@ -24,6 +21,27 @@ describe('pos', function () {
 
   });
 
+  it('should print an empty inventory when get a empty array input', function(){
+
+    var inputs = ['ITEM000002'];
+
+    spyOn(console, 'log');
+
+    printInventory(inputs);
+
+    var expectText =
+      '***<没钱赚商店>购物清单***\n' +
+      '名称：苹果，数量：1斤，单价：5.50(元)，小计：5.50(元)\n' +
+      '----------------------\n' +
+      '挥泪赠送商品：\n' +
+      '----------------------\n' +
+      '总计：5.50(元)\n' +
+      '节省：0.00(元)\n' +
+      '**********************';
+
+    expect(console.log).toHaveBeenCalledWith(expectText);
+
+  });
 
 /*
     it('should print correct text', function () {
--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -1,10 +1,37 @@
 function printInventory(barcodeList) {
+  allItems = loadAllItems();
+
+  var shoppingCart = [];
+  var sumPrice = 0.0;
+
+  allItems.forEach(function(item){
+    barcodeList.forEach(function(barcode){
+      if(barcode === item.barcode){
+        shoppingCart.push({itemInfo:item, amount:1});
+        sumPrice += item.price;
+      }
+    });
+  });
+
+  var itemsOutput = "";
+
+  // var comma = "，";
+
+  shoppingCart.forEach(function(item){
+    itemsOutput += "名称：" + item.itemInfo.name + "，" +
+    "数量：" + item.amount + item.itemInfo.unit + "，" +
+    "单价：" + item.itemInfo.price.toFixed(2) + "(元)，" +
+    "小计：" + (item.itemInfo.price * item.amount).toFixed(2) + "(元)\n";
+  });
+
+
   var expectText =
     '***<没钱赚商店>购物清单***\n' +
+    itemsOutput +
     '----------------------\n' +
     '挥泪赠送商品：\n' +
     '----------------------\n' +
-    '总计：0.00(元)\n' +
+    '总计：' + sumPrice.toFixed(2) + '(元)\n' +
     '节省：0.00(元)\n' +
     '**********************';
 
